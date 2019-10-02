13ee53296ce42b7df7dd7b15c949c027bbd3d38c
pangyunsheng <434497112@qq.com>
2019-10-02 16:02:09 +0800

    modify tree

--- Tree/tree.py
+++ Tree/tree.py
@@ -16,17 +16,16 @@ class Tree(object):
             self.root = node
             self.queue.append(node)
         else:
-            while True:
-                point = self.queue[0]
-                if point.left == None:
-                    point.left = node 
-                    self.queue.append(node)
-                    return
-                elif point.right == None:
-                    point.right = node
-                    self.queue.pop(0)
-                    self.queue.append(node)
-                    return
+            point = self.queue[0]
+            if point.left == None:
+                point.left = node 
+                self.queue.append(node)
+                return
+            elif point.right == None:
+                point.right = node
+                self.queue.pop(0)
+                self.queue.append(node)
+                return
 
     # 前序遍历(递归)
     def pre_order_recursive(self, root, res=[]):
@@ -55,18 +54,60 @@ class Tree(object):
     def in_order_recursive(self, root, res=[]):
         if not root:
             return
-        self.pre_order_recursive(root.left,res)
+        self.in_order_recursive(root.left,res)
         res.append(root.val)
-        self.pre_order_recursive(root.right,res)
+        self.in_order_recursive(root.right,res)
         return res
 
     # 中序遍历(迭代)
-    def in_order_iterative(self, root, res=[]):
+    def in_order_iterative(self, root, res=[], stack=[]):
         if not root:
             return
-        self.pre_order_recursive(root.left,res)
+        node = root
+        while stack or node:
+            while node:
+                stack.append(node)
+                node=node.left
+            node=stack.pop()
+            res.append(node.val)
+            node=node.right
+        return res
+
+    # 后序遍历(递归)
+    def post_order_recursive(self, root, res=[]):
+        if not root:
+            return
+        self.post_order_recursive(root.left,res)
+        self.post_order_recursive(root.right,res)
         res.append(root.val)
-        self.pre_order_recursive(root.right,res)
+        return res
+
+    # 后序遍历(迭代)
+    def post_order_iterative(self, root, res=[], stack=[]):
+        stack.append(root)
+        while stack:
+            node = stack.pop()
+            if node.left:
+                stack.append(node.left)
+            if node.right:
+                stack.append(node.right)
+            res.append(node.val)
+        return res[::-1]
+
+    # 层序遍历
+    def level_order(self, root, res=[], stack=[]):
+        if not root:
+            return
+        stack.append(root)
+        res.append(root.val)    
+        while stack != []:
+            node = stack.pop(0)
+            if node.left != None:
+                res.append(node.left.val)
+                stack.append(node.left)
+            if node.right != None:
+                res.append(node.right.val)
+                stack.append(node.right)
         return res
 
 
@@ -78,13 +119,23 @@ if __name__ =='__main__':
         print('add node ' + str(i))
 
     res_pre_recu = tree.pre_order_recursive(tree.root)
-    print(res_pre_recu)
+    print("pre order with recursiving: ", res_pre_recu)
 
     res_pre_iter = tree.pre_order_iterative(tree.root)
-    print(res_pre_iter)
+    print("pre order with iterating: ", res_pre_iter)
 
-    in_pre_recu = tree.in_order_recursive(tree.root)
-    print(in_pre_recu)
+    res_in_recu = tree.in_order_recursive(tree.root)
+    print("pre order with recursiving: ", res_in_recu)
 
-    in_pre_iter = tree.in_order_iterative(tree.root)
-    print(in_pre_iter)
+    res_in_iter = tree.in_order_iterative(tree.root)
+    print("pre order with iterative: ", res_in_iter)
+
+    res_post_recu = tree.post_order_recursive(tree.root)
+    print("post order with recursive: ", res_post_recu)
+
+    res_post_iter = tree.post_order_iterative(tree.root)
+    print("post order with iterative: ", res_post_iter)
+
+    res_level = tree.level_order(tree.root)
+    print("level order: ", res_level)
+
