c97e3dc0cc2160a24197ebf910c55091664a31c8
pangyunsheng <434497112@qq.com>
2019-10-21 18:14:04 +0800

    add leetcode 040

---
+++ Back Tracking/040_Combination_Sum_II.py
@@ -0,0 +1,23 @@
+class Solution:
+    def combinationSum2(self, candidates, target: int):
+        if not candidates: return []
+        n = len(candidates)
+        candidates = sorted(candidates)
+        path = []
+        res = []
+        
+        def dfs(begin, target):
+            if target == 0:
+                res.append(path[:])
+                
+            for index in range(begin, n):
+                if index > begin and candidates[index] == candidates[index-1]: continue
+                residue = target - candidates[index]
+                if residue < 0:
+                    break
+                path.append(candidates[index])
+                dfs(index+1, residue)
+                path.pop()
+                
+        dfs(0, target)
+        return res
