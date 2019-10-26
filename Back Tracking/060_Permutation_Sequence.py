e75530a4ac773c76ef4d4d26c1acf8b29c4a3c02
pangyunsheng <434497112@qq.com>
2019-10-26 10:39:58 +0800

    add leetcode 060

--- Back Tracking/060_Permutation_Sequence.py
+++ Back Tracking/060_Permutation_Sequence.py
@@ -21,14 +21,37 @@
 #         back_tracking()
 #         return ''.join([str(i) for i in res[k-1]])
 
-def frac(n):
-    if n == 0: return 0
-    res = 1
-    temp = n
-    while temp > 0:
-        res *= temp
-        temp -= 1
-    return res
-
-a = frac(0)
-print(a)
+# 方法二：回溯+剪枝
+class Solution:
+    def getPermutation(self, n: int, k: int) -> str:
+        if n == 1: return "1"
+        path = []
+        
+        def frac(n):
+            if n == 0: return 0
+            res = 1
+            temp = n
+            while temp > 0:
+                res *= temp
+                temp -= 1
+            return res
+        
+        def back_tracking(level, temp_k):
+            for i in range(1, n+1):
+                if i in path:
+                    continue
+                
+                value = frac(n-level)
+                if value == 0:
+                    path.append(i)
+                    return 
+                
+                if temp_k > value:
+                    temp_k = temp_k - value
+                    continue
+                
+                path.append(i)
+                back_tracking(len(path)+1, temp_k)
+                
+        back_tracking(1, k)
+        return ''.join([str(i) for i in path])
