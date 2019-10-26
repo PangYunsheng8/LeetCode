df3b7fd9d73a3fa72cece34139d906e900b72fb8
pangyunsheng <434497112@qq.com>
2019-10-26 12:10:36 +0800

    add leetcode 144

--- Tree/144_Binary_Tree_Preorder_Traversal.py
+++ Tree/144_Binary_Tree_Preorder_Traversal.py
@@ -21,3 +21,22 @@ class Solution:
         _preorderTraversal(root)
         
         return res
+
+
+class Solution:
+    def preorderTraversal(self, root: TreeNode):
+        if not root: return []
+        
+        res = []
+        stack = [root]
+        while stack:
+            node = stack.pop()
+            res.append(node.val)
+            
+            if node.right:
+                stack.append(node.right)
+                
+            if node.left:
+                stack.append(node.left)
+        
+        return res
