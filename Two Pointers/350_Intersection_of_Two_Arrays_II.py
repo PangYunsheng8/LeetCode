e2a019762bf4afb4ceced5c5792221550f162300
YunshengPang <434497112@qq.com>
2019-10-28 23:25:53 +0800

    add leetcode 350

--- Two Pointers/350_Intersection_of_Two_Arrays_II.py
+++ Two Pointers/350_Intersection_of_Two_Arrays_II.py
@@ -1,5 +1,5 @@
 class Solution:
-    def intersection(self, nums1, nums2):
+    def intersect(self, nums1, nums2):
         nums1 = sorted(nums1)
         nums2 = sorted(nums2)
         res = []
@@ -9,13 +9,10 @@ class Solution:
             if nums1[p1] == nums2[p2]:
                 res.append(nums1[p1])
                 p1 += 1
-                while p1 < len(nums1) and nums1[p1] == nums1[p1-1]: p1 += 1
                 p2 += 1
-                while p2 < len(nums2) and nums2[p2] == nums2[p2-1]: p2 += 1
             elif nums1[p1] < nums2[p2]:
                 p1 += 1
             else:
                 p2 += 1
-                
+        
         return res
-            
