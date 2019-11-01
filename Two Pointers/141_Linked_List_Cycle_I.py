b71598a924a6698b3b52860f344ff756c7d40917
YunshengPang <434497112@qq.com>
2019-11-01 12:14:55 +0800

    modify leetcode 141

--- Two Pointers/141_Linked_List_Cycle_I.py
+++ Two Pointers/141_Linked_List_Cycle_I.py
@@ -22,19 +22,29 @@
 #         return False
 
 # 方法二：快慢指针
-class Solution(object):
-    def hasCycle(self, head):
-        """
-        :type head: ListNode
-        :rtype: bool
-        """
-        if not head or head.next == None:
-            return False
-        slow = head
-        fast = head.next
-        while slow != fast:
-            if fast == None or fast.next == None:
-                return False
-            slow = slow.next
+# class Solution(object):
+#     def hasCycle(self, head):
+#         """
+#         :type head: ListNode
+#         :rtype: bool
+#         """
+#         if not head or head.next == None:
+#             return False
+#         slow = head
+#         fast = head.next
+#         while slow != fast:
+#             if fast == None or fast.next == None:
+#                 return False
+#             slow = slow.next
+#             fast = fast.next.next
+#         return True
+
+# 还是快慢指针，换了个写法而已，上面是参考别人的，下面是自己写的
+class Solution:
+    def hasCycle(self, head) -> bool:
+        slow, fast = head, head
+        while fast:
+            if not fast.next or not fast.next.next: return False
             fast = fast.next.next
-        return True
+            slow = slow.next 
+            if slow == fast: return True
