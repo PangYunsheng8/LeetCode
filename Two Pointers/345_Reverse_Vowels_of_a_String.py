f32d6ca9110381158d088cf1ed7d5f2d861713de
YunshengPang <434497112@qq.com>
2020-01-17 13:53:45 +0800

    add leetcode 345

---
+++ Two Pointers/345_Reverse_Vowels_of_a_String.py
@@ -0,0 +1,19 @@
+class Solution:
+    def reverseVowels(self, s: str) -> str:
+        vowel = ["a", "e", "i", "o", "u", "A", "E", "I", "O", "U",]
+        n = len(s)
+        left, right = 0, n - 1
+        s_list = list(s)
+        while left < right:
+            if s_list[left] not in vowel and left < n - 1: left += 1
+            if s_list[right] not in vowel and right > 0: right -= 1
+            print(left, right)
+            if left < right:
+                s_list[left], s_list[right] = s_list[right], s[left]
+                left += 1
+                right -= 1
+        return ''.join(s_list)
+
+model = Solution()
+res = model.reverseVowels("a.b,.")
+print(res)
