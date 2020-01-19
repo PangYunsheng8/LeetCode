7314c9a3b9727eb9457f32c9e0cacd4396181b56
YunshengPang <434497112@qq.com>
2020-01-19 14:45:17 +0800

    modify leetcode 167

--- Array/167_Two_Sum_II-Input_array_is_sorted.py
+++ Array/167_Two_Sum_II-Input_array_is_sorted.py
@@ -3,8 +3,6 @@ class Solution:
         left, right = 0, len(numbers) - 1
         while left < right:
             while numbers[left] + numbers[right] > target and left < right: right -= 1
-            print(left, right)
             while numbers[left] + numbers[right] < target and left < right: left += 1
-            print(left, right)
             if numbers[left] + numbers[right] == target:
                 return [left+1, right+1]
