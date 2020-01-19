feca6889c33eb052924386d5eb5349fabbbb4a43
YunshengPang <434497112@qq.com>
2020-01-19 14:29:46 +0800

    modify leetcode 054

--- Array/054_Spiral_Matrix.py
+++ Array/054_Spiral_Matrix.py
@@ -15,4 +15,26 @@ class Solution:
             t += 1
             b -= 1
         return res    
+
+
+class Solution:
+    def spiralOrder(self, matrix: List[List[int]]) -> List[int]:
+        if not matrix: return []
+
+        mat = []
+        m = len(matrix)
+        n = len(matrix[0])
+        l, r, t, b = 0, n - 1, 0, m - 1
+        while len(mat) < n * m:
+            for i in range(l, r + 1): mat.append(matrix[t][i])
+            t += 1
+            for i in range(t, b + 1): mat.append(matrix[i][r])
+            r -= 1
+            if b > t - 1 and r + 1 > l:
+                for i in range(r, l - 1, -1): mat.append(matrix[b][i])
+                b -= 1
+                for i in range(b, t - 1, -1): mat.append(matrix[i][l])
+                l += 1
+        return mat
+
         
