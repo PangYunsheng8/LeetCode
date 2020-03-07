7ec44ec0262a991eff3be360f26b34688164d78e
YunshengPang <434497112@qq.com>
2020-03-07 14:40:49 +0800

    add 01back

--- Dynamic Programming/back/1_01back.py
+++ Dynamic Programming/back/1_01back.py
@@ -7,7 +7,8 @@
 
 # 初始化: f[0][0] = 0
 
-class ZeroOneBack:
+# 二维DP,时间复杂度O(NV), 空间复杂度O(NV)
+class ZeroOneBack2D:
     def zoback(self, N: int, Volume: int, V: list, W: list) -> int:
         m, n = N, Volume
         dp = [[0 for _ in range(n+1)] for _ in range(m+1)] # 注意这里把N和Volume都加了1，是为了考虑没有item和体积为0的情况。
@@ -18,12 +19,25 @@ class ZeroOneBack:
                     dp[i][j] = max(dp[i][j], dp[i - 1][j - V[i-1]] + W[i-1])
         return dp[-1][-1]
 
+# 一维DP,时间复杂度O(NV),空间复杂度O()
+class ZeroOneBack1D:
+    def zoback(self, N: int, Volume: int, V: list, W:list) -> int:
+        m, n = N, Volume
+        dp = [0 for _ in range(n+1)]
+        for i in range(m):
+            for j in range(n, -1, -1):
+                if j >= V[i]:
+                    dp[j] = max(dp[j], dp[j-V[i]] + W[i])
+                print(dp[j])
+        print(dp)
+        return dp[-1]
 
 if __name__ == '__main__':
     N, Volume = 4, 5
     V = [1, 2, 3, 4]
     W = [2, 4, 4, 5]
 
-    model = ZeroOneBack()
+    # model = ZeroOneBack2D()
+    model = ZeroOneBack1D()
     ans = model.zoback(N, Volume, V, W)
     print(ans)
